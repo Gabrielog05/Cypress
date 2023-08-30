df480e07dee91d804426929da6338ed94d1b41ed
Gabrielog05 <gabrieldeoliveira05@gmail.com>
2023-08-30 14:26:15 -0300

    Refazendo

--- alurapic/cypress/e2e/login-incorreto.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Página de Login', () => { //Nome do cenário de teste
-    it('Verificar mensagem de campos obrigatórios', () => {// Nome do caso de teste
-      cy.visit('http://localhost:4200/#/home');          
-      cy.contains('User name is required!').should('be.visible');
-      cy.contains('Password is required!').should('be.visible');
-    })
-  })
--- alurapic/cypress/support/commands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('logando', (nome, senha) => {
-    cy.get('[data-test="loginUserName"]').type(nome)
-    cy.get('[data-test="loginPassword"]').type(senha)
-    cy.contains('button', 'login').click()    
-})
-
